757ba3e976cb8c1c2f1a1d4672f9e40642a70514
guilhermeabraob <guilhermeabraob@hotmail.com>
2022-03-30 18:55:10 -0300

    Commit M13

---
+++ Módulo 13/Script M13.js
@@ -0,0 +1,42 @@
+Exercício Módulo 13
+
+1)
+
+10 + 15 = 25 (Number)
+"10" + 2 = "102" (String)
+"10" * 2 = 20 (Number)
+"10" / 3 = 3.33 (Number)
+"10" % 3 = 1 (Number)
+10 + true = 11 (Number)
+10  == "10" = true
+10 === "10" = false
+10 < 11 = true
+10 > 12 = false
+10 <= 10.1 = true
+10 > 9.99 = true
+10 != "dez" = true
+10 + true = 11 (Number)
+"dez" + true = deztrue (String) 
+10 + false = 10 (Number)
+10 * false = 0 (Number)
+true + true = 2 (Number)
+10++ = (Incremento)
+10-- = (Decremento)
+1 & 1 = 1 (Number)
+1 & 0 = 0 (Number)
+0 & 0 = 0 (Number)
+1 & 0 = 0 (Number)
+0 / 1 = 0 (Number)
+5 + 5 == 10 = true
+"5"+"5" == 10 = false
+"5" * 2 > 9 = true
+(10+10)*2 = 40 (Number)
+10 + 10 * 2 = 30 (Number)
+
+2)
+a) true
+b) true
+c) false
+d) 
+e) 
+f) pretobrancocinza = 16 caracteres
